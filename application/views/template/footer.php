e58cf3a6e7c2fe2d0cda21386b22edc07a4aba82
purnamalie <purnamasupardilie07@gmail.com>
2023-11-12 21:50:15 +0700

    build_cover

---
+++ application/views/template/footer.php
@@ -0,0 +1,11 @@
+<footer class="main-footer">
+</footer>
+</div>
+
+<script src="<?php echo base_url(); ?>assets/plugins/jQuery/jquery-3.5.1.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/toaster/js/toastr.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/script.js"></script>
+</body>
+
+</html>
